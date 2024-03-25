1ea0f1f193c25cddf2df9aa47d9cb7905c399e86
punsiwooot <punsiwoot@gmail.com>
2024-03-25 15:53:54 +0700

    update

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import json 
 prun_vgg16 = open('prun_vgg16.json')
 pretraine_vgg16 = open('pretrain_vgg16.json')
-pretraine_Resnet50 = open('prun_vgg16.json')
+pretraine_Resnet50 = open('pretrain_Resnet50.json')
 pretraine_InceptionV3 = open('prun_vgg16.json')
-cfg = json.load(pretraine_vgg16)
+cfg = json.load(pretraine_Resnet50)
